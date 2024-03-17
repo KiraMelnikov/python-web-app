cd191b8a45748087d78e6eb0ca026576bc3f7856
k.melnikov <k.melnikov@temabit.com>
2024-03-17 13:09:30 +0200

    added folders

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import requests
 import os 
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
+city = input("\nEnter city name:\n")
+url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&&units=metric'
 
-url = f'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={os.getenv("API_KEY")}'
+response = requests.get(url=url).json()
 
-response = requests.get(url=url)
-
-print(response.json())
+print(json.dumps(response, indent=2))
