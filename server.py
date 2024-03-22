27184c514e2f1aa7301d70b8f04ad7781720f96b
k.melnikov <k.melnikov@temabit.com>
2024-03-22 15:31:32 +0200

    delete geo

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from waitress import serve
-from geo_service_API import ConnectionsAPI as geoApi
 try:
     from weather import Weather as w
 except:
@@ -35,15 +34,6 @@ def getting_weather():
         feels_like= f"{weather_data['main']['feels_like']:.1f}"
     )
 
-@app.route('/geo_km')
-def geo_km():
-    result = geoApi().get_request(50.467995,30.877765,49.754654,31.459351) 
-    return render_template(
-        'geo_km.html',
-        var_km = str(result["trip"]["summary"]["length"]) + " km",
-        var_cost = str(result["trip"]["summary"]["cost"]) + " cost",
-        var_time = str(result["trip"]["summary"]["time"]) + " time"
-        )
 
 if __name__=='__main__':
     serve(app, host="0.0.0.0", port=8000)
